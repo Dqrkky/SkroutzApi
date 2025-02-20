9d78c98e9d1bfb0854c45f728c48e89850d6225c
Dqrkky <staurospapadogiannopoulos@gmail.com>
2025-02-20 03:26:18 +0200

    master

--- test.py
+++ test.py
@@ -2,15 +2,16 @@ import skroutz
 import asyncio
 import decodeshit
 import cookies
-import html_to_json
 import json
+import os
 
 def dapdtj(html):
+    os.system("cls")
     print(
         json.dumps(
-            obj=decodeshit.call(
+            obj=[{"name": sku["name"], "price": sku["price"], "image_url": sku["image_url"], "url": f'https://skroutz.gr{sku["sku_url"]}'} for sku in decodeshit.call(
                 html=html
-            ),
+            )[0]["initialData"]["data"]["skus"]],
             indent=4
         )
     )
@@ -21,19 +22,14 @@ async def run():
         cookies.Cookie("cookies.skroutz.pkl") as cook:
             if cook.exists:
                 sk.scraper.cookies.update(cook.read())
-            html=sk.getJson(
-                sk.search("pc psu")
-            ).content
-            dapdtj(html)
-            cloudflare = [i["meta"] for i in html_to_json.convert(html_string=html)["html"][0]["head"][0]["meta"] if "meta" in i]
-            if len(cloudflare) > 0:
-                sk.scraper.headers.update({
-                    "x-csrf-token": cloudflare[0][0]["meta"][0]["_attributes"]["content"]
-                })
-            html=sk.getJson(
-                sk.search("mobile screen replacement")
-            ).content
-            dapdtj(html)
+            try:
+                while True:
+                    html=sk.getJson(
+                        sk.search(input("Search :"))
+                    ).content
+                    dapdtj(html)
+            except KeyboardInterrupt:
+                exit()
             data = sk.scraper.cookies.get_dict()
             data.pop("__cf_bm")
             cook.write(data)
